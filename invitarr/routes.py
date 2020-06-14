4666426f49aebbafbb4b43297cf6f5f6b734d2eb
sahil97 <sahil.sharma071997@gmail.com>
2020-06-14 21:23:03 +0530

    added favicon

--- invitarr/routes.py
+++ invitarr/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash
 
 from invitarr import app
-from invitarr.forms import BotForm
+from invitarr.forms import BotForm, PlexForm
 from invitarr import configHandler
 
 @app.route('/', methods=['GET'])
@@ -10,11 +10,22 @@ def home():
 
 @app.route('/bot', methods=['GET', 'POST'])
 def bot():
-    env_form = BotForm()
-    if env_form.validate_on_submit():
+    form = BotForm()
+    if form.validate_on_submit():
         try:
-            configHandler.change_config_form(env_form)
+            configHandler.change_config_form(form)
             flash('Settings updated.')
         except:
             flash('Some error in updating settings')
-    return render_template('bot.html', form = env_form)
+    return render_template('bot.html', form = form)
+
+@app.route('/plex', methods=['GET', 'POST'])
+def plex():
+    form = BotForm()
+    if form.validate_on_submit():
+        try:
+            configHandler.change_config_form(form)
+            flash('Settings updated.')
+        except:
+            flash('Some error in updating settings')
+    return render_template('bot.html', form = form)
