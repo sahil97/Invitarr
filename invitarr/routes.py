2c39bb77ceffdd8e34e7d229e7637d01638be204
sahil97 <sahil.sharma071997@gmail.com>
2020-06-14 20:38:41 +0530

    completed bot envs form

--- invitarr/routes.py
+++ invitarr/routes.py
@@ -1,9 +1,17 @@
-from flask import render_template
+from flask import render_template, flash
 
 from invitarr import app
-from invitarr.forms import EnvForm
+from invitarr.forms import BotForm
+from invitarr import config
 
 @app.route('/', methods=['GET'])
 def home():
-    env_form = EnvForm()
-    return render_template('index.html', form = env_form)
+    return render_template('index.html')
+
+@app.route('/bot', methods=['GET', 'POST'])
+def bot():
+    env_form = BotForm()
+    if env_form.validate_on_submit():
+        flash('Settings updated.')
+        print(env_form)
+    return render_template('bot.html', form = env_form)
