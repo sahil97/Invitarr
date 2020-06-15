691c34d3e8f3d0b6ff7a856ebfcbfa04261c69c6
sahil97 <sahil.sharma071997@gmail.com>
2020-06-15 20:58:08 +0530

    added login roues

--- invitarr/forms.py
+++ invitarr/forms.py
@@ -2,6 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired
 
+class LoginForm(FlaskForm):
+
+    username = StringField('Username',
+                                validators=[DataRequired()])
+
+    password =  PasswordField('Password',
+                                validators=[DataRequired()])
+
+    submit = SubmitField('Save Changes')
+
 class GeneralForm(FlaskForm):
     
     username = StringField('Username',
--- invitarr/routes.py
+++ invitarr/routes.py
@@ -1,19 +1,44 @@
-from flask import render_template, flash
+from flask import render_template, flash, request, redirect, url_for
+from flask_login import login_user, current_user, logout_user, login_required
 
-from invitarr import app
-from invitarr.forms import GeneralForm, BotForm, PlexForm
+from invitarr import app, db, bcrypt
+from invitarr.models import User
+from invitarr.forms import LoginForm, GeneralForm, BotForm, PlexForm
 from invitarr import configHandler
+
+db.create_all()
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=True)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password')
+    return render_template('login.html', title='Login', form=form)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = GeneralForm()
-    if form.validate_on_submit():
-        try:
-            configHandler.change_config_form(form)
-            flash('Settings updated.')
-        except:
-            flash('Some error in updating settings')
-    return render_template('index.html', form = form)
+    if request.method == 'GET':
+        user = User.query.all()[0]
+        form.username.data = user.username
+        form.password.data = user.password
+    elif request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.all()[0]
+            hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+            user.password = hashed_password
+            db.session.commit()
+            flash('Details updated.')
+            return redirect(url_for('login'))
+    return render_template('index.html', form=form)
 
 @app.route('/bot', methods=['GET', 'POST'])
 def bot():
